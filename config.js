b5e0529e6c2181d26492c0edd396fc7ce7e34f44
Johan Back <johan.backs@gmail.com>
2020-12-14 17:14:45 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteTitle: 'Portfolio Johan', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Johan',
+  lastName: 'Back',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/johanbacks',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://linkedin.com/in/johan-back',
+    },
+   
+  ],
+  email: 'johan.backs@gmail.com',
+  address: 'Curitiba, Paraná, BR',
+};
